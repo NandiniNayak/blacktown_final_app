f4d25370df74b9710f6cb1ad65cb60487fb1a642
Nandini Nayak <nandini@coderfactory.com>
2016-11-23 01:34:34 +0000

    class 2- subjects added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121030348) do
+ActiveRecord::Schema.define(version: 20161122221910) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20161121030348) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "faculty"
+    t.string   "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
